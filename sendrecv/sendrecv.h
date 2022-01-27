41d01ee919990c7b15217541b7063e9f83c5d389
Betül Öztürk <86080047+betuloztrk@users.noreply.github.com>
2022-01-27 20:39:02 +0300

    Add files via upload

---
+++ sendrecv/sendrecv.h
@@ -0,0 +1,22 @@
+
+
+#ifndef SENDRECV_SENDRECV_H_
+#define SENDRECV_SENDRECV_H_
+
+/* İncludes **************************************************************/
+#include <stdint.h>
+#include <stdio.h>
+#include "../msgprotocol/msgprotocol.h"
+
+/* Functions **************************************************************/
+
+
+extern void sendrecv_send(protocol_t *packet,int socket_desc,char *serialize);
+//extern void sendrecv_recv(protocol_t *packet,int socket_desc,char *serialize);
+
+
+
+//extern int comd,id;
+
+
+#endif /* SENDRECV_SENDRECV_H_ */
